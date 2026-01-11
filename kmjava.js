6e95913a692e6b08c24717a9b5a1490dbfe232f3
Tempura <107868499+Tempura-Server@users.noreply.github.com>
2026-01-11 19:00:41 +0900

    Create kmjava.js

---
+++ kmjava.js
@@ -0,0 +1,9 @@
+var km = false;
+var fire = true;
+setInterval(function() {
+  if(fire){if(km){document.getElementById("kageyama").innerText = "☆☆☆☆☆☆☆☆☆";}
+else{document.getElementById("kageyama").innerText = "★★★★★★★★★";}
+   km = !km; }},1000);
+
+function fires() {fire = false;document.getElementById("kageyama").innerText = "";};
+function firea() {fire = true;document.getElementById("kageyama").innerText = "★★★★★★★★★";};
