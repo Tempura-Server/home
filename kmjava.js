c39fe3a2becee77d15b04259636540e4d7ba806f
Tempura <107868499+Tempura-Server@users.noreply.github.com>
2026-01-11 21:06:59 +0900

    Update kmjava.js

--- kmjava.js
+++ kmjava.js
@@ -3,7 +3,7 @@ var fire = true;
 setInterval(function() {
   if(fire){if(km){document.getElementById("kageyama").innerText = "☆☆☆☆☆☆☆☆☆";}
 else{document.getElementById("kageyama").innerText = "★★★★★★★★★";}
-   km = !km; }},1000);
+   km = !km; }},500);
 
 function fires() {fire = false;document.getElementById("kageyama").innerText = "";};
 function firea() {fire = true;document.getElementById("kageyama").innerText = "★★★★★★★★★";};
